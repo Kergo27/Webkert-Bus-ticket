c9d01b0399cba537fc75bd8bbdac45f6f1b1b793
Kergo27 <kocsis.gergo@icloud.com>
2023-05-07 11:47:29 +0200

    asd

---
+++ src/app/user/models/route.model.ts
@@ -0,0 +1,10 @@
+/*export class Journey_Route {
+    leaving_form:string;
+    going_to:string;
+    date:string;
+}*/
+export class Journey_Route {
+  leaving_from: string;
+  going_to: string;
+  date: string;
+}
